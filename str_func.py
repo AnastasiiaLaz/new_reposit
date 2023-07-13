eb0004f88a402c69e7cc30f45ee4927845e27fb7
nasty <nastyastep246@gmail.com>
2023-07-13 18:22:14 +0300

    new function

--- str_func.py
+++ str_func.py
@@ -1,6 +1,18 @@
+"""
+функция выводит слово капсом
+"""
+
+
 def upper_word():
     word = input()
     return word.upper()
 
 
-print(upper_word())
+"""
+функция выводит слово с загловной буквы
+"""
+
+
+def tittle_word():
+    word = input()
+    return word.title()
